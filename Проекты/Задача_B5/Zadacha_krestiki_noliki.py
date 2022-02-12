9bb91a333a392bbfdff8fb11cbcbaf5761476903
Evgeniy Arkchipov <spbrba@rambler.ru>
2022-02-13 01:11:49 +0300

    Vvedenie-v-OOP

--- Проекты/Задача_B5/Zadacha_krestiki_noliki.py
+++ Проекты/Задача_B5/Zadacha_krestiki_noliki.py
@@ -1,9 +1,13 @@
-a = [1, 4, 5, 7, 8]
-def kvadrat(x):
-    return x**2
-
-c = list(map(kvadrat, a))
-print(c)
-
-d = list(map(lambda x: x+2, a))
-print(d)
+class Rotata:
+    name = "Жопа"
+    age = 31
+a = Rotata()
+print(Rotata.name)
+print(a.__dict__)
+print(Rotata.__dict__)
+Rotata.weight = 73
+print(Rotata.__dict__)
+a.b = 1000
+print(a.b)
+setattr(Rotata, "name", "Жопааа")
+print(Rotata.__dict__)
